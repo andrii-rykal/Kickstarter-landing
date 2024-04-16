50aafeed82cde4744808167d8d7dccb985efe389
Andrii Rykal <andreuka79@gmail.com>
2024-04-16 11:31:59 +0300

    make build

---
+++ index.c2773399.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".questions__form");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.c2773399.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const form = document.querySelector(".questions__form");
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    form.reset();
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
